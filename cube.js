33522c8ef25a5c5cbf245bd572634047b32c3573
PMR85 <permagnus.roenning@gmail.com>
2025-08-15 16:06:45 +0200

    Update cube.js

--- cube.js
+++ cube.js
@@ -75,8 +75,8 @@ function makeFaceMaterial(label, opts = {}) {
 
   return new THREE.MeshStandardMaterial({
     map: tex,
-    metalness: 0.15, // ↑ more mirror-like if higher (0..1)
-    roughness: 0.22  // ↓ shinier if lower (0..1)
+    metalness: 0.35, // ↑ more mirror-like if higher (0..1)
+    roughness: 0.12  // ↓ shinier if lower (0..1)
   });
 }
 
